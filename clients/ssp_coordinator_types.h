ed30c700a8618ba5cfd9530598a5758672807438
André Mourão <andre.b.mourao@gmail.com>
2019-10-08 18:10:05 +0100

    Adding skeleton class for the SSP Coordinator

---
+++ clients/ssp_coordinator_types.h
@@ -0,0 +1,53 @@
+//
+// Created by amourao on 08/10/19.
+//
+
+#pragma once
+
+enum ExchangeDataType {
+  EXCHANGE_DATA_TYPE_VECTOR_FRAME_STRUCT = 0,
+  EXCHANGE_DATA_TYPE_VECTOR_CV_MAT,
+  EXCHANGE_DATA_TYPE_VECTOR_POINTCLOUD,
+  EXCHANGE_DATA_TYPE_K4A_CAPTURE,
+  EXCHANGE_DATA_TYPE_VECTOR_SKELETON
+};
+
+enum FrameSourceType {
+  FRAME_SOURCE_ANY = 0,
+  FRAME_SOURCE_KINECT_DK,
+  FRAME_SOURCE_VIDEO,
+  FRAME_SOURCE_FRAMES,
+  FRAME_SOURCE_CAMERA
+};
+
+enum ExecStatus {
+  EXEC_STATUS_STOPPED = 0,
+  EXEC_STATUS_WAITING,
+  EXEC_STATUS_RUNNING
+};
+
+struct ProcessorInstance {
+  std::string id;
+  FrameSourceType source_type;
+  ExchangeDataType data_type;
+  std::string metadata;
+  std::string host;
+  std::string port;
+  ExecStatus status;
+};
+
+struct FrameServerInstance {
+  std::string id;
+  FrameSourceType source_type;
+  std::string metadata;
+  std::string host;
+  std::string port;
+  ExecStatus status;
+};
+
+struct FrameServerProcessorConnection {
+  std::string id;
+  ProcessorInstance processor;
+  FrameServerInstance frameserver;
+  ExecStatus status;
+};
