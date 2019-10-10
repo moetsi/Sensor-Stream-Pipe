9ed4674187b2177ea58034fb6498c5e7cd705264
André Mourão <andre.b.mourao@gmail.com>
2019-10-10 10:14:35 +0100

    WIP: creating state managing classes

---
+++ servers/ssp_server.h
@@ -0,0 +1,33 @@
+//
+// Created by amourao on 10/10/19.
+//
+
+#pragma once
+
+#include "../clients/ssp_coordinator_types.h"
+#include <string>
+
+class SSPServer {
+private:
+  BrokerInstance broker_;
+  BrokerInstance coordinator_;
+
+  ExecStatus status_;
+
+public:
+  SSPServer();
+  ~SSPServer();
+
+  int ConnectBroker(const std::string &host, const int &port,
+                    std::string &error);
+
+  int ConnectCoordinator(std::string &error);
+
+  int GetBroker(BrokerInstance &connection, std::string &error);
+
+  int GetCoordinator(BrokerInstance &connection, std::string &error);
+
+  int Start(std::string &error);
+
+  int Stop(std::string &error);
+};
