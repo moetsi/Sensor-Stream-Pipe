6b4db127d0b347351ee2714bdb4133983bb4633a
David Geldreich <david.geldreich@free.fr>
2021-06-08 20:00:50 +0200

    package only library for use with Unity

--- cmake/package_for_unity.sh
+++ cmake/package_for_unity.sh
@@ -6,5 +6,10 @@ echo "Using ${CONFIG}"
 
 rm -rf ssp_plugin_unity
 mkdir ssp_plugin_unity
+# Copy opencv2 and ffmpeg frameworks without the headers
 cp -R _deps/ssp_dep-src/lib/* ssp_plugin_unity
+rm -rf ssp_plugin_unity/opencv2.framework/Versions/A/Headers
+rm -rf ssp_plugin_unity/opencv2.framework/Headers
+rm -rf ssp_plugin_unity/FFmpeg.framework/Headers
+find _deps -name "*.a" -print0 | xargs -0 -I % cp % ssp_plugin_unity
 cp lib/${CONFIG}/*.a ssp_plugin_unity
