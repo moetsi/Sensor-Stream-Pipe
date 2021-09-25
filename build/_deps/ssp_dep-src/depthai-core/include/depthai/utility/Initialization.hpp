4554c5199ffa67231f7a07b9d32dfc2b7d68ebd3
adammpolak <adammpolak@gmail.com>
2021-09-24 21:43:58 -0400

    able to pull deptai::opencv and run deptha:: and have oakd_xlink_reader available, need to test existing executables

--- build/_deps/ssp_dep-src/depthai-core/include/depthai/utility/Initialization.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace dai {
-
-bool initialize(std::string additionalInfo = "");
-
-}  // namespace dai
--- build/_deps/ssp_dep-src/zdepth/cmake/zdepth-config.cmake
+++
@@ -1,2 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/zdepth.cmake)
-get_filename_component(zdepth_TARGET_DIR "${CMAKE_CURRENT_LIST_DIR}" PATH)
